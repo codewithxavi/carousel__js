fc4e4e941373bec4c828c9402823d7d568e56e81
xavicoder <javieradrian.it@gmail.com>
2021-09-26 23:11:17 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// Grab DOM elements
+const images = document.querySelector(".images")
+const imagesCollection = document.querySelectorAll(".image")
+const prevBtn = document.querySelector(".prev")
+const nextBtn = document.querySelector(".next")
+
+prevBtn.addEventListener("click", prevSlide)
+nextBtn.addEventListener("click", nextSlide)
+
+
+// Utility vars
+let index = 0
+
+// function startInterval() {
+//   index++
+//   moveCarousel()
+// }
+
+// Set interval
+let interval = setInterval(startInterval, 1500)
+
+function startInterval() {
+  index++
+  moveCarousel()
+}
+
+function resetInterval() {
+  clearInterval(interval)
+  interval = setInterval(startInterval, 1500)
+}
+
+function moveCarousel() {
+  if (index > imagesCollection.length - 1) {
+    index = 0
+  } else if (index < 0) {
+    index = imagesCollection.length - 1
+  }
+  images.style.transform = `translateX(-${index * 800}px)`
+}
+
+// Nav buttons
+function prevSlide() {
+  index--
+  resetInterval()
+  moveCarousel()
+}
+
+function nextSlide() {
+  index++
+  resetInterval()
+  moveCarousel()
+}
